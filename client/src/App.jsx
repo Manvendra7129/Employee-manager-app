885c091610d90c7494f3d1e74d298f5378c55b0b
manvendra pratap singh <manvendra7129@gmail.com>
2024-05-05 16:32:22 +0530

    basic components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './Component/Login/Login'
+import "./App.css";
+import Login from "./Component/Login/Login";
+import ProjectCards from "./Component/Cards/ProjectCards";
+import ProfessionalData from "./Component/ProfessionalData/ProfessionalData";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       <Login></Login>
-    </>
-  )
+    <div>
+      {/* <Login /> */}
+      {/* <ProjectCards /> */}
+      <ProfessionalData />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/Component/Cards/ProjectCards.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import style from "./ProjectCards.module.css";
+import ProjectCard from "./ProjectCard";
+const ProjectCards = () => {
+  return (
+    <div className={style.demo_div}>
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+      <ProjectCard
+        name="salman"
+        project="ems"
+        start="31/08/2000"
+        end="31/08/2000"
+        discription=" An employee management app streamlines HR tasks, from scheduling to
+        performance tracking, in one accessible platform."
+      />
+    </div>
+  );
+};
+
+export default ProjectCards;
